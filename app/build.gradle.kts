c4d79cdbec85b33e34c74ce210cada9c24208963
TacoTakumi <195555+TacoTakumi@users.noreply.github.com>
2026-03-28 21:10:19 -0300

    bump version to 1.1.2 (code 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.pocketscope"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
